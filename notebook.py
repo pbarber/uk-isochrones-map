ab1014c7a3d7cc5dc5f8d120da16c0addfbf7cca
Paul Barber <paul_j_barber@yahoo.co.uk>
2024-01-12 22:31:51 +0000

    Collect stats on connected SAs

--- notebook.py
+++ notebook.py
@@ -2,6 +2,7 @@
 import geopandas
 import pandas
 from pyproj import Transformer
+import re
 
 def get_iso_centre(row, dim):
     if row['iso_type'] == 'from_centroid':
@@ -28,9 +29,6 @@ df['Travel Minutes'] = pandas.to_numeric(df['iso_cutoff'] / 60, downcast='intege
 df['iso_centre_X'] = df.apply(get_iso_centre, axis=1, dim='X')
 df['iso_centre_Y'] = df.apply(get_iso_centre, axis=1, dim='Y')
 
-# %%
-df.head()
-
 # %% Reduce down the size of the dataframe and then split by Small Area to minimise data accesses from the app
 def get_difference(a, b):
     if a.geometry.isna().all() or b.geometry.isna().all():
@@ -47,16 +45,32 @@ for sa in reduced['SA2011'].unique():
     pandas.concat([min60, min45, min30, min15]).to_file(f'{sa}.geojson', driver='GeoJSON')
 
 # %%
-df.groupby('iso_type').count()
+# Load the Small Areas boundaries, preconverted to match geometries
+sa2011 = geopandas.read_file('sa2011_epsg4326_simplified15.json')
+
+# Find all SAs that are accessible from each SA
+joined = df.sjoin(sa2011, how='inner', predicate='intersects',lsuffix='from',rsuffix='to')[['SA2011_from','SA2011_to','Travel Minutes']]
 
 # %%
-original = reduced[reduced['SA2011']=='N00000897']
-min60 = original.loc[lambda df: df['Travel Minutes'] == 60, :].overlay(original.loc[lambda df: df['Travel Minutes'] == 45, :], how='difference')
-min45 = original.loc[lambda df: df['Travel Minutes'] == 45, :].overlay(original.loc[lambda df: df['Travel Minutes'] == 30, :], how='difference')
-min30 = original.loc[lambda df: df['Travel Minutes'] == 30, :].overlay(original.loc[lambda df: df['Travel Minutes'] == 15, :], how='difference')
-min15 = original.loc[lambda df: df['Travel Minutes'] == 15, :]
-pandas.concat([min60, min45, min30, min15]).to_file('N00000897.geojson', driver='GeoJSON')
+# Get the Small Area populations for 2020
+pops = pandas.read_excel('SAPE20-SA-Totals.xlsx', sheet_name='Flat')
+pops = pops[(pops['Area']=='Small Areas') & (pops['Year']==2020)][['Area_Code','MYE']]
+joined = joined.merge(pops, how='inner', left_on='SA2011_from', right_on='Area_Code')
+joined = joined.merge(pops, how='inner', left_on='SA2011_to', right_on='Area_Code', suffixes=['_from','_to'])
 
+# %%
+nimdm = pandas.read_excel('NIMDM17_SA%20-%20for%20publication.xls', sheet_name='MDM')
+nimdm.columns = nimdm.columns.str.replace(re.compile('\(.+'), '', regex=True).str.replace('\n','').str.strip()
+joined = joined.merge(nimdm, how='left', left_on='SA2011_from', right_on='SA2011').drop(columns=['SA2011'])
 
+# %%
+nimdm = pandas.read_excel('NIMDM17_SA%20-%20for%20publication.xls', sheet_name='Income')
+nimdm.columns = nimdm.columns.str.replace(re.compile('\(.+'), '', regex=True).str.replace('\n','').str.strip()
+joined = joined.merge(nimdm[['SA2011','Proportion of the population living in households whose equivalised income is below 60 per cent of the NI median']], how='left', left_on='SA2011_from', right_on='SA2011').drop(columns=['SA2011'])
+
+# %%
+nimdm = pandas.read_excel('NIMDM17_SA%20-%20for%20publication.xls', sheet_name='Employment')
+nimdm.columns = nimdm.columns.str.replace(re.compile('\(.+'), '', regex=True).str.replace('\n','').str.strip()
+joined = joined.merge(nimdm[['SA2011','Proportion of the working age population who are employment deprived']], how='left', left_on='SA2011_from', right_on='SA2011').drop(columns=['SA2011'])
 
 # %%
