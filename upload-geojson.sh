5b7eed592993e8332f009ef22fa0c1fe1fc37840
Paul Barber <paul_j_barber@yahoo.co.uk>
2024-01-21 14:09:35 +0000

    Added upload script

---
+++ upload-geojson.sh
@@ -0,0 +1,17 @@
+aws --profile ${AWS_PROFILE} s3 cp --recursive East\ Midlands/ s3://${BUCKETNAME}
+aws --profile ${AWS_PROFILE} s3 cp --recursive East\ Scotland/ s3://${BUCKETNAME}
+aws --profile ${AWS_PROFILE} s3 cp --recursive East\ of\ England/ s3://${BUCKETNAME}
+aws --profile ${AWS_PROFILE} s3 cp --recursive London\ East/ s3://${BUCKETNAME}
+aws --profile ${AWS_PROFILE} s3 cp --recursive London\ West/ s3://${BUCKETNAME}
+aws --profile ${AWS_PROFILE} s3 cp --recursive North\ East/ s3://${BUCKETNAME}
+aws --profile ${AWS_PROFILE} s3 cp --recursive North\ Scotland/ s3://${BUCKETNAME}
+aws --profile ${AWS_PROFILE} s3 cp --recursive North\ West\ North/ s3://${BUCKETNAME}
+aws --profile ${AWS_PROFILE} s3 cp --recursive North\ West\ South/ s3://${BUCKETNAME}
+aws --profile ${AWS_PROFILE} s3 cp --recursive Northern\ Ireland/ s3://${BUCKETNAME}
+aws --profile ${AWS_PROFILE} s3 cp --recursive South\ East/ s3://${BUCKETNAME}
+aws --profile ${AWS_PROFILE} s3 cp --recursive South\ West/ s3://${BUCKETNAME}
+aws --profile ${AWS_PROFILE} s3 cp --recursive Wales/ s3://${BUCKETNAME}
+aws --profile ${AWS_PROFILE} s3 cp --recursive West\ Midlands/ s3://${BUCKETNAME}
+aws --profile ${AWS_PROFILE} s3 cp --recursive West\ Scotland\ North/ s3://${BUCKETNAME}
+aws --profile ${AWS_PROFILE} s3 cp --recursive West\ Scotland\ South/ s3://${BUCKETNAME}
+aws --profile ${AWS_PROFILE} s3 cp --recursive Yorkshire\ and\ the\ Humber/ s3://${BUCKETNAME}
